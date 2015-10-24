e9d8ba2bbaaf0e050935c63f82fafa400c1c6ace
gam0022 <gam0022@gmail.com>
2015-10-25 05:23:07 +0900

    さっきのところを RIHGT JOINにしてみた(11443.0)

--- go/app.go
+++ go/app.go
@@ -346,7 +346,7 @@ func GetIndex(w http.ResponseWriter, r *http.Request) {
 	rows, err = db.Query(`SELECT c.id AS id, c.entry_id AS entry_id, c.user_id AS user_id, c.comment AS comment, c.created_at AS created_at,
 u.account_name, u.nick_name
 FROM comments c
-JOIN entries e ON c.entry_id = e.id
+RIGHT JOIN entries e ON (c.entry_id = e.id)
 LEFT JOIN users AS u ON (c.user_id = u.id)
 WHERE e.user_id = ?
 ORDER BY c.created_at DESC
